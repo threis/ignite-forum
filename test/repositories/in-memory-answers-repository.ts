2fa2de40f9c87b1d2879355fd2395c1cb7fdbd20
Thiago <thiagoreis93@gmail.com>
2024-06-12 20:19:38 -0300

    add delete answer use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -2,8 +2,23 @@ import { AnswersRepository } from '@/domain/forum/application/repositories/answe
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
+  async findById(id: string) {
+    const answer = await this.items.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
   public items: Answer[] = []
   async create(answer: Answer) {
     this.items.push(answer)
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+    this.items.splice(itemIndex, 1)
   }
 }
