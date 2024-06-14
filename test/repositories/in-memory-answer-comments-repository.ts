baf621b0f3ad5707138a4346a653194700f3c8b5
Thiago <thiagoreis93@gmail.com>
2024-06-13 21:12:41 -0300

    add delete answer comment use case

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -6,7 +6,26 @@ export class InMemoryAnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  async findById(id: string) {
+    const answerComment = await this.items.find(
+      (item) => item.id.toString() === id,
+    )
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+    this.items.splice(itemIndex, 1)
   }
 }
